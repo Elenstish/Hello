83534e0a4366a6ef2de103755e45dbbda6e57bab
Elenstish <elenstish@gmail.com>
2018-02-06 23:15:17 +0200

    plus jquery

--- script.js
+++ script.js
@@ -2,11 +2,7 @@ var sidebar = $('.post-aside');
 var btn = $('button.buttonmobile');
 var maxFont = 24;
 var minFont = 8;
-var downFont = $('#downFont');
-var upFont = $('#upFont');
-var myFontSize = $('p');
-
-$(document).ready(function() { 
+var myFontFamily = $('p');
 
 // ---------sidebar
 
@@ -14,32 +10,32 @@ $(document).ready(function() {
         sidebar.toggleClass('active');
     });
 
-// ------- font-size + -
+// ------- font-size 
 
-    upFont.click(function (event) {
+    $('#userSize').change(function(event){
         event.preventDefault();
-        var fontSize = $('p').css('font-size');
-        var newSize = parseInt(fontSize) + 1;
-        newSize = newSize > maxFont ? maxFont: newSize;
-        $('p').css('font-size', newSize + 'px');
-        fontSize.change();
+        var thisValue = parseInt($(this).val());
+        var newSize = $(this).val();
+        console.log(newSize);
+         //onkeydown(function)
+        if (newSize > 24 || newSize < 8) {
+            alert('from 8 to 24 only')
+            return;
         }
-    ); 
-
-    downFont.click(function () {
-        event.preventDefault();
-        var fontSize = $('p').css('font-size');
-        var newSize = parseInt(fontSize) - 1;
-        newSize = newSize < minFont ? minFont: newSize;
         $('p').css('font-size', newSize + 'px');
-        fontSize.change();
         }
     );
 
-//--------onclick radio myFontSize
 
-    $('input[name=mySize]').on('change', function() {
-       myFontSize.removeClass().addClass($(this).val());
+        function clearText(thefield){
+        if (thefield.defaultValue==thefield.value) 
+            thefield.value = "";
+    }
+    
+//--------onclick radio myFontFamily
+
+    $('input[name=myFont]').on('change', function() {
+       myFontFamily.removeClass().addClass($(this).val());
     });
 
 // -----  remove last p-element-----
@@ -47,4 +43,3 @@ $(document).ready(function() {
     $('#pfade').click(function() {
         $('p').last().remove();
     });
-})
