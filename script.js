c891c5e94a85a2a4bd32a885901e34a9c076c2fb
Elenstish <elenstish@gmail.com>
2018-02-07 17:42:07 +0200

    task-2 js not optimised

--- script.js
+++ script.js
@@ -2,44 +2,61 @@ var sidebar = $('.post-aside');
 var btn = $('button.buttonmobile');
 var maxFont = 24;
 var minFont = 8;
-var myFontFamily = $('p');
 
 // ---------sidebar
 
-    btn.on('click', function(){
-        sidebar.toggleClass('active');
-    });
+btn.on('click', function(){
+    sidebar.toggleClass('active');
+});
 
 // ------- font-size 
 
-    $('#userSize').change(function(event){
-        event.preventDefault();
-        var thisValue = parseInt($(this).val());
-        var newSize = $(this).val();
-        console.log(newSize);
-         //onkeydown(function)
-        if (newSize > 24 || newSize < 8) {
-            alert('from 8 to 24 only')
-            return;
-        }
-        $('p').css('font-size', newSize + 'px');
-        }
-    );
-
-
-        function clearText(thefield){
-        if (thefield.defaultValue==thefield.value) 
-            thefield.value = "";
+$('#userSize').change(function(event){
+    event.preventDefault();
+    var thisValue = parseInt($(this).val());
+    var newSize = thisValue;
+    
+    if (newSize > maxFont || newSize < minFont) {
+    alert('input number from 8 to 24 only')
+    return;
     }
+
+    $('p').css('font-size', newSize + 'px'); 
+
+});
+
+$('#userSize').keydown(function(e){
+    if (e.which < 48 || e.which > 57) {
+        alert('integer only')
+        event.preventDefault();
+    }    
+});
+
+$('#userSize').focus(function(){
+    elementValue = $(this).val();
+    $(this).val("");
+});
+
     
 //--------onclick radio myFontFamily
 
-    $('input[name=myFont]').on('change', function() {
-       myFontFamily.removeClass().addClass($(this).val());
-    });
+$('input[name=myFont]').on('change', function() {
+    $('p').removeClass().addClass($(this).val());
+});
 
-// -----  remove last p-element-----
+// -----  remove last p-element
 
-    $('#pfade').click(function() {
-        $('p').last().remove();
-    });
+$('#pfade').click(function() {
+    $('p').last().remove();
+});
+
+// -----------Color
+
+$('#input_color_value').change(function(event){
+    event.preventDefault();
+    var value = $(this).val();
+    var result = value;
+    
+    $('body').css('background-color', result); 
+
+});
